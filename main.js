dc7782c79cdf9b56c5cc3fe7c89fbf913ec3158e
Margo Mezenok <margomezenok@gmail.com>
2019-08-15 14:46:00 +0300

    product_details

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-$(".dropdown_container").click(function(e) {
-	// предотвратить сплытие события к родителям
-	e.stopPropagation();
-	// удалить класс .active у всех уже активных элементов кроме текущего
- 	$(".dropdown_container.active").not(this).removeClass("active");
- 	// добавить текущему элементу класс .active
- 	$(this).toggleClass("active");
-});
+$(document).ready(function(){
+	$(".dropdown_container").click(function(e) {
+		// предотвратить сплытие события к родителям
+		e.stopPropagation();
+		// удалить класс .active у всех уже активных элементов кроме текущего
+	 	$(".dropdown_container.active").not(this).removeClass("active");
+	 	// добавить текущему элементу класс .active
+	 	$(this).toggleClass("active");
+	});
 
+	$('.minus-btn').on('click', function(e) {
+	    e.preventDefault();
+	    var $this = $(this);
+	    var $input = $this.closest('div').find('input');
+	    var value = parseInt($input.val());
+	 
+	    if (value > 1) {
+	        value = value - 1;
+	    } else {
+	        value = 0;
+	    }
+	 
+	  $input.val(value);
+	 
+	});
+	 
+	$('.plus-btn').on('click', function(e) {
+	    e.preventDefault();
+	    var $this = $(this);
+	    var $input = $this.closest('div').find('input');
+	    var value = parseInt($input.val());
+	 
+	    if (value < 100) {
+	        value = value + 1;
+	    } else {
+	        value =100;
+	    }
+	 
+	    $input.val(value);
+	});
+})
